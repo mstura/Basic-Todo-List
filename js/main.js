2120d7bfadfee6c39f5295f8da7b891c9e0c71aa
mstura <mstura@outlook.com>
2016-11-03 16:05:57 +0100

    code correction
    fixed a misuse of Filter with find

--- js/main.js
+++ js/main.js
@@ -9,8 +9,11 @@
   app.completeButton = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 function ButtonIdentifier (e){
 	if (e.target !== e.currentTarget) {
-    var newTargets = e.path.filter(determineTarget);
-    var t = newTargets[0];
+    var t = e.path.find(function (trg){
+      if (trg.tagName) {
+        return trg.tagName.toLowerCase() === 'button';
+      }});
+
     if (t) {
         if (t.className === "deleteB") {
                 removeElement(t.parentNode.id);
@@ -20,14 +23,6 @@ function ButtonIdentifier (e){
             }
     }
 	e.stopPropagation();
-}
-
-function determineTarget (t){
-  if (t.tagName) {
-    if (t.tagName.toLowerCase() === 'button') {
-    return t;
-    }
-  }
 }
 
 function addElement(){
